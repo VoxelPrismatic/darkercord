408b921003cdc3dd26c90822cd858d4c3abaaf42
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-06-30 10:58:01 -0500

    Update inst-linux.sh

--- script/inst-linux.sh
+++ script/inst-linux.sh
@@ -1,4 +1,4 @@
-cp newcss.css ~/snap/discord/current/
-cp newjs.css ~/snap/discord/current/
+cp ../newcss.css ~/snap/discord/current/
+cp ../newjs.css ~/snap/discord/current/
 python inst-py.py "/home/$(whoami)/snap/discord/current/.config/discord/0.0.10/modules/discord_utils/"
 echo "true" > ~/snap/discord/current/loadcss.bool
