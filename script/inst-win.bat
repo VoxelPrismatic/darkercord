87dc604720ec3366a65073a2f5d08b1f8f3214a3
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-06-30 10:57:43 -0500

    Update inst-win.bat

--- script/inst-win.bat
+++ script/inst-win.bat
@@ -1,1 +1,6 @@
-
+COPY ../newjs.js %APPDATA%\discord
+COPY ../newcss.css %APPDATA%\discord
+ECHO. >> %APPDATA%\discord\0.0.306\modules\discord_utils\index.js
+ECHO require^("../../../newjs.js"^); >> %APPDATA%\discord\0.0.306\modules\discord_utils\index.js
+ECHO true > %APPDATA%\discord\loadcss.bool
+ECHO DARKERcord installed
