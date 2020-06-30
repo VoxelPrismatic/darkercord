e1a84395008c8ed9d8e9932caf816cbee10305e6
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-06-30 08:53:58 -0500

    Update inst-linux.sh

--- script/inst-linux.sh
+++ script/inst-linux.sh
@@ -1,7 +1,3 @@
 cp newcss.css ~/snap/discord/current/
 cp newjs.css ~/snap/discord/current/
-{
-    python inst-py3.py "/home/$(whoami)/snap/discord/current/.config/discord/0.0.10/modules/discord_utils/"
-} || {
-    python inst-py2.py "/home/$(whomai)/snap/discord/current/.config/discord/0.0.10/modules/discord_utils/"
-}
+python inst-py.py "/home/$(whoami)/snap/discord/current/.config/discord/0.0.10/modules/discord_utils/"
