e78855b5f1c0316baea2632bd02b711c3ae6568c
VoxelPrismatic <danielpetrov@outlook.com>
2020-07-17 16:42:52 -0500

    v0.9

--- script/inst-linux.sh
+++ script/inst-linux.sh
@@ -1,32 +1,29 @@
+echo -e "\e[94;1mInstalling DARKERcord...\e[0m"
+
 discord=~/snap/discord/current/
-cp ../darker_css.css $discord
-cp ../darker_js.js $discord
-cp ../darker_settings.html $discord
-cp ../darker_update.html $discord
-cp ../request $discord -r
 indexjs=~/snap/discord/current/.config/discord/0.0.10/modules/discord_utils/index.js
-case $(cat $indexjs) in
-    *require\(\"\.\./\.\./\.\./\.\./\.\./newjs.js\"\)* ) {
-        echo "Cleaning up after DARKERcord prior to v0.5"
-        head -n -3 $indexjs > $indexjs;
-    }
-esac;
+
+{
+    echo -e "\e[90m> Copying files\e[0m"
+    cp ../darker_css.css $discord
+    cp ../darker_js.js $discord
+    cp ../darker_settings.html $discord
+    cp ../darker_update.html $discord
+    echo -e "\e[90m> Copying dependencies\e[0m"
+    cp ../node_stuff/node_modules $discord -r
+    cp ../node_stuff/package-lock.json $discord
+} || {
+    echo -e "\e[41;30mYou aren't in the right folder, please run this script ./script folder\e[0m"
+    exit
+}
+
+echo -e "\e[90m> Setting up\e[0m"
+cp ../index-backup.js $indexjs -T
+cp $indexjs $indexjs\~ -T
+echo "" >> $indexjs
+echo "require(\"../../../../../darker_js.js\");" >> $indexjs
 
 vnum=$(cat $discord\darker_js.js | grep "__version_number" | head -n 1);
 vnum=$(echo $vnum | head -c -3 | tail -c +25);
 
-case $(cat $indexjs) in
-    *require\(\"\.\./\.\./\.\./\.\./\.\./darker_js.js\"\)* ) {
-        echo "DARKERcord v$vnum already installed";
-        html=$(curl -s https://github.com/voxelprismatic/darkercord/releases | grep -E "/VoxelPrismatic/darkercord/tree/v[[:digit:]]+\.[[:digit:]]+" | head -n 1)
-        html=$(echo $html | head -c -3 | tail -c +89)
-        echo "DARKERcord $html is the latest stable version";
-        echo "https://github.com/voxelprismatic/darkercord/releases/latest"
-    };
-esac;
-
-cp $indexjs $indexjs\~ -T
-echo "" >> $indexjs
-echo "require(\"../../../../../darker_js.js\");" >> $indexjs
-echo
-echo "DARKERcord installed"
+echo -e "\e[94;1mDARKERcord v$vnum installed\e[0m"
--- script/inst-win.bat
+++ script/inst-win.bat
@@ -1,24 +1,28 @@
-COPY ..\darker_js.js %APPDATA%\discord\
-COPY ..\darker_css.css %APPDATA%\discord\
-COPY ..\darker_settings.html %APPDATA%\discord\
-COPY ..\darker_update.html %APPDATA%\discord\
-MKDIR %APPDATA%\discord\request
-XCOPY ..\request\* %APPDATA%\discord\request /E
+@ECHO off
+COLOR 0B
+CLS
 
-IF (FIND "require(\"../../../newjs.js\")" %APPDATA%\discord\0.0.306\modules\discord_utils\index.js) GTR "" (
-    ECHO Cleaning up after DARKERcord prior to v0.5
-    FINDSTR /R /I /V "^$require(^"^.^./^.^./^.^./newjs^.js^")" %APPDATA%\discord\0.0.306\modules\discord_utils\index.js >> %APPDATA%\discord\0.0.306\modules\discord_utils\index.js
-)
+ECHO Installing DARKERcord...
 
-COPY %APPDATA%\discord\0.0.306\modules\discord_utils\index.js %APPDATA%\discord\0.0.306\modules\discord_utils\index.js.000
+SET "DISCORD=%APPDATA%\discord\"
+SET "INDEXJS=%APPDATA%\discord\0.0.306\modules\discord_utils\index.js"
 
-IF (FIND "require(\"../../../darker_js.js\")" %APPDATA%\discord\0.0.306\modules\discord_utils\index.js) GTR "" (
-    ECHO DARKERcord already installed
-    ECHO NOTE: This installer cannot check for updates,
-    ECHO To check for updates, please go to Discord Settings ^> OS Settings ^> Check for DARKERcord updates
-) ELSE (
-    ECHO. >> %APPDATA%\discord\0.0.306\modules\discord_utils\index.js
-    ECHO require^("../../../darker_js.js"^); >> %APPDATA%\discord\0.0.306\modules\discord_utils\index.js
-    ECHO DARKERcord installed
-)
-PAUSE > NUL
+ECHO ^> Copying files
+COPY ..\darker_js.js %DISCORD%
+COPY ..\darker_css.css %DISCORD%
+COPY ..\darker_settings.html %DISCORD%
+COPY ..\darker_update.html %DISCORD%
+
+ECHO ^> Copying dependencies
+COPY ..\node_stuff\package-lock.json %DISCORD%
+MKDIR %APPDATA%\discord\node_modules
+XCOPY ..\node_stuff\node_modules\* %DISCORD%node_modules /E
+
+ECHO ^> Setting up
+COPY ..\index-backup.js %INDEXJS%
+COPY %INDEXJS% %INDEXJS%.000
+ECHO. >> %INDEXJS%
+ECHO require^("../../../darker_js.js"^); >> %INDEXJS%
+
+ECHO DARKERcord installed
+PAUSE
