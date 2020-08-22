7d69ba3d9112fa8b63ac5c1a5cd2debf9a4df869
PRIZ ;] <prizmdev@outlook.com>
2020-08-21 20:41:58 -0500

    Update inst-win10.bat

--- script/inst-win10.bat
+++ script/inst-win10.bat
@@ -1,14 +1,13 @@
 @ECHO off
 TITLE PRIZcord
-CALL :ESCAPE
 CLS
 
-ECHO %ESC%[94;1mInstalling PRIZcord...%ESC%[0m
+TYPE ESC.TXT & ECHO [94;1mInstalling PRIZcord... & TYPE ESC.TXT & ECHO [0m
 
 SET "DISCORD=%APPDATA%\discord\"
 SET "INDEXJS=%APPDATA%\discord\0.0.307\modules\discord_utils\index.js"
 
-ECHO %ESC%[90m^> Copying files%ESC%[0m
+TYPE ESC.TXT & ECHO [90m^> Copying files & TYPE ESC.TXT & ECHO [0m
 COPY ..\darker_js.js %DISCORD% > NUL 2> NUL
 IF %ERRORLEVEL% NEQ 0 GOTO ERR
 MKDIR %DISCORD%darker_themes > NUL 2> NUL
@@ -18,12 +17,11 @@ XCOPY ..\darker_html\* %DISCORD%darker_html /E /Y > NUL 2> NUL
 MKDIR %DISCORD%darker_tray > NUL 2> NUL
 XCOPY ..\darker_tray\* %DISCORD%darker_tray /E /Y > NUL 2> NUL
 
-ECHO %ESC%[90m> Copying dependencies%ESC%[0m
+TYPE ESC.TXT & ECHO [90m^> Copying dependencies & TYPE ESC.TXT & ECHO [0m
 COPY ..\node_stuff\package-lock.json %DISCORD% > NUL 2> NUL
 MKDIR %DISCORD%node_modules > NUL 2> NUL
 XCOPY ..\node_stuff\node_modules\* %DISCORD%node_modules /E /Y > NUL 2> NUL
-
-ECHO %ESC%[90m> Setting up%ESC%[0m
+TYPE ESC.TXT & ECHO [90m^> Setting up & TYPE ESC.TXT & ECHO [0m
 COPY ..\index-backup.js %INDEXJS% > NUL 2> NUL
 COPY %INDEXJS% %INDEXJS%.000 > NUL 2> NUL
 ECHO. >> %INDEXJS%
@@ -34,17 +32,10 @@ SET /P __DARKER_VERSION__=<__DARKER_VERSION__.TXT
 SET "VNUM=%__DARKER_VERSION__:~24,-2%"
 DEL __DARKER_VERSION__.TXT
 
-ECHO %ESC%[94;1mPRIZcord v%VNUM% installed%ESC%[0m
+TYPE ESC.TXT & ECHO [94;1mPRIZcord v%VNUM% installed & TYPE ESC.TXT & ECHO [0m
 PAUSE
 EXIT
 
 :ERR
 ECHO %ESC%[41;30;1mYou aren't in the right folder, please run this script ./script folder%ESC%[0m
 EXIT
-
-:ESCAPE
-FOR /F "tokens=1,2 delims=#" %%T IN ('"PROMPT #$H#$E# & @ECHO on & FOR %%E IN (1) DO REM"') DO (
-  SET ESC=%%E
-  EXIT /B 0
-)
-@ECHO off
